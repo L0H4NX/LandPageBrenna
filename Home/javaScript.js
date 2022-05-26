091a30beff18b97c955fbeaba2be2b1513499e8a
L0H4NX <lsteferson83@gmail.com>
2022-05-26 16:35:54 -0300

    atualização projeto dia 26/05/2022

--- Home/javaScript.js
+++ Home/javaScript.js
@@ -1,18 +1,18 @@
-function teste(){
-    var btn = document.getElementById('hamburguer')
-    var modal =  document.getElementById('conteiner-modal')
-    var cabecario = document.querySelector('.conteiner-cabecario')
+// function teste(){
+//     var btn = document.getElementById('hamburguer')
+//     var modal =  document.getElementById('conteiner-modal')
+//     var cabecario = document.querySelector('.conteiner-cabecario')
 
-    modal.style.opacity = "1"
-    cabecario.style.display= "none"
-}
+//     modal.style.opacity = "1"
+//     cabecario.style.display= "none"
+// }
 
 
-function closeModal(){
-    var modal =  document.getElementById('conteiner-modal')
-    var cabecario = document.querySelector('.conteiner-cabecario')
+// function closeModal(){
+//     var modal =  document.getElementById('conteiner-modal')
+//     var cabecario = document.querySelector('.conteiner-cabecario')
 
-    modal.style.opacity='0'
+//     modal.style.opacity='0'
 
-    cabecario.style.display = "flex"
-}
+//     cabecario.style.display = "flex"
+// }
