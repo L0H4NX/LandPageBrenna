02c21972ea73446ecb579d6cac0a7f8438e390b8
Steferson Lohan Farias Furtado <87098947+L0H4NX@users.noreply.github.com>
2022-06-08 18:34:15 -0300

    atualização projeto 08/06/2022

---
+++ javaScript.js
@@ -0,0 +1,27 @@
+$('.carrosel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    centerMode: true,
+    variableWidth: true,
+    infinite: true,
+    autoplay: true,
+
+  });    
+  
+  $('.conteiner-carrosel-depoimentos').slick({
+
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    adaptiveHeight: true,
+  });
+
+
+
+
